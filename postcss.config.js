d315df9070b4403e9568b55e9ae9b10e82489a0d
Mohit <mohit.peak@gmail.com>
2024-05-19 18:54:37 +0530

    setup tailwind

---
+++ postcss.config.js
@@ -0,0 +1,13 @@
+// postcss.config.js
+import autoprefixer from "autoprefixer";
+import tailwindcss from "tailwindcss";
+// import other plugins
+
+export default {
+  plugins: [
+    autoprefixer,
+    tailwindcss,
+
+    // other plugins
+  ],
+};
